32c13c9ea6088afe65db14e0382d185b63b45100
Valentin Aebi <valentin.aebi@epfl.ch>
2022-03-08 11:30:01 +0100

    Main setting activity with account section

---
+++ app/src/main/java/com/github/multimatum_team/multimatum/MainSettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.github.multimatum_team.multimatum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainSettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_settings)
+    }
+}
+
