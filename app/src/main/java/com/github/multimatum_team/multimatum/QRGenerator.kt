61d2aaea226a63d692c6e7d9c400d8f5d30ec7d6
Florian Delavy <Florian.delavy@epfl.ch>
2022-03-08 10:19:36 +0100

    Finished code Generator QR

---
+++ app/src/main/java/com/github/multimatum_team/multimatum/QRGenerator.kt
@@ -0,0 +1,51 @@
+package com.github.multimatum_team.multimatum
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.qrcode.QRCodeWriter
+
+class QRGenerator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrgenerator)
+    }
+
+    fun returnMain(view: View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+
+    fun displayQRCode(view: View){
+        val qrNameField = findViewById<EditText>(R.id.QRTextEdit)
+
+        val data = qrNameField.text.toString().trim()
+
+        if(data.isEmpty()){
+            Toast.makeText(this, "Enter some data", Toast.LENGTH_SHORT).show()
+        } else {
+            val writer = QRCodeWriter()
+
+
+            val bitmap = writer.encode(data, BarcodeFormat.QR_CODE, 512, 512)
+            val width = bitmap.width
+            val height = bitmap.height
+            val bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+            for (x in 0 until width){
+                for(y in 0 until height){
+                    bmp.setPixel(x,y, if (bitmap[x,y]) Color.BLACK else Color.WHITE)
+                }
+            }
+            findViewById<ImageView>(R.id.QRGenerated).setImageBitmap(bmp)
+        }
+
+    }
+}
