a54662c01132d8045feb9d0277b60d32e7c0b40f
Léo Hauser <leo.hauser@epfl.ch>
2022-03-14 21:43:34 +0100

    Adding changing activity test

--- app/src/androidTest/java/com/github/multimatum_team/multimatum/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/multimatum_team/multimatum/MainActivityTest.kt
@@ -1,17 +1,22 @@
 package com.github.multimatum_team.multimatum
 
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
+import org.hamcrest.Matchers
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -33,9 +38,6 @@ class MainActivityTest {
 
     /*
     @get:Rule
-    val permissionRule: GrantPermissionRule = GrantPermissionRule.grant(
-        android.Manifest.permission.CAMERA
-    )
 
     @Test
     fun should_switch_to_qr_code_activity_if_permission_granted() {
@@ -45,4 +47,22 @@ class MainActivityTest {
         intended(hasComponent(QRCodeReaderActivity::class.java.name))
     }
     */
+
+    @get:Rule
+    val permissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        android.Manifest.permission.CAMERA
+    )
+
+    @Test
+    fun goToQrCodeReader() {
+        Intents.init()
+        Espresso.onView(ViewMatchers.withId(R.id.mainGoButton)).perform(ViewActions.click())
+        Intents.intended(
+            Matchers.allOf(
+                IntentMatchers.hasComponent(QRCodeReaderActivity::class.java.name),
+                IntentMatchers.toPackage("com.github.multimatum_team.multimatum")
+            )
+        )
+        Intents.release()
+    }
 }
