355572138e732fe05a974138e7fa929c9afed47b
Valentin Aebi <valentin.aebi@epfl.ch>
2022-03-17 20:12:30 +0100

    Describe the role of MultimatumApp.kt in a comment

--- app/src/main/java/com/github/multimatum_team/multimatum/MultimatumApp.kt
+++ app/src/main/java/com/github/multimatum_team/multimatum/MultimatumApp.kt
@@ -6,6 +6,8 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class MultimatumApp: Application() {
 
-
+    // This class is required by Hilt
+    // And code generation seems to be very sensitive to changes in the format of this file
+    // tl;dr: BLACK MAGIC, DO NOT TOUCH
 
 }
