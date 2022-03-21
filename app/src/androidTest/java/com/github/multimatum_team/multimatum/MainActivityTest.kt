8d54b09b8d25787a4327c20d6251f4c387f95467
Florian Delavy <Florian.delavy@epfl.ch>
2022-03-21 16:39:23 +0100

    Last modifications to allow any tests with robolectric

--- app/src/androidTest/java/com/github/multimatum_team/multimatum/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/multimatum_team/multimatum/MainActivityTest.kt
@@ -8,6 +8,7 @@ import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
 import dagger.Module
 import dagger.Provides
@@ -21,6 +22,7 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
 
 @UninstallModules(DependenciesProvider::class)
