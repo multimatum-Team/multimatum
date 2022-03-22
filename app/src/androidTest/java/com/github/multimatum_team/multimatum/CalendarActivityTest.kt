574d533b3acea889c48060fbe051b3f96ee04a59
Léo Hauser <leo.hauser@epfl.ch>
2022-03-22 20:29:32 +0100

    Adding calendar view tests 5

--- app/src/androidTest/java/com/github/multimatum_team/multimatum/CalendarActivityTest.kt
+++ app/src/androidTest/java/com/github/multimatum_team/multimatum/CalendarActivityTest.kt
@@ -1,6 +1,8 @@
 package com.github.multimatum_team.multimatum
 
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -17,4 +19,15 @@ class CalendarActivityTest {
         Espresso.onView(ViewMatchers.withId(R.id.calendar_view))
             .check(ViewAssertions.matches(ViewMatchers.isEnabled()))
     }
+    /*
+    @Test
+    fun addingDeadlineOpensListviewIfNotAlreadyTheCase() {
+        Espresso.onView(ViewMatchers.withId(R.id.textInputEditCalendar))
+            .perform(ViewActions.typeText("Test"))
+        Espresso.onView(ViewMatchers.withId(R.id.calendar_add_deadline_button))
+            .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.calendar_deadline_listview))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+     */
 }
