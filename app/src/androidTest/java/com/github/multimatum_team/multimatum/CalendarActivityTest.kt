eca44e762afd6b28d3e146a6ca0e12c3b11bafe4
Léo Hauser <leo.hauser@epfl.ch>
2022-03-22 19:02:39 +0100

    Adding calendar view tests 2

---
+++ app/src/androidTest/java/com/github/multimatum_team/multimatum/CalendarActivityTest.kt
@@ -0,0 +1,20 @@
+package com.github.multimatum_team.multimatum
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import org.junit.Test
+
+class CalendarActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(CalendarActivity::class.java)
+
+    @Test
+    fun shouldDisplayCalendarAtOpening() {
+        Espresso.onView(ViewMatchers.withId(R.id.calendar_view))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+}
--- app/src/main/java/com/github/multimatum_team/multimatum/CalendarActivity.kt
+++ app/src/main/java/com/github/multimatum_team/multimatum/CalendarActivity.kt
@@ -5,14 +5,11 @@ import android.os.Bundle
 import android.view.View
 import android.widget.CalendarView
 import android.widget.ListView
-import androidx.core.view.get
 import com.github.multimatum_team.multimatum.model.Deadline
 import com.github.multimatum_team.multimatum.model.DeadlineAdapter
 import com.github.multimatum_team.multimatum.model.DeadlineState
 import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 import java.time.Instant
-import java.time.LocalDate
 import java.time.ZoneId
 
 class CalendarActivity : AppCompatActivity() {
@@ -22,7 +19,7 @@ class CalendarActivity : AppCompatActivity() {
         initCalendar()
     }
 
-    private fun initCalendar(){
+    private fun initCalendar() {
         setContentView(R.layout.activity_calendar)
     }
 
@@ -31,7 +28,7 @@ class CalendarActivity : AppCompatActivity() {
     deadline easily using directly the calendar to select the date.
     ***TO DO***: store deadline and display them on the calendar.
      */
-    fun addNewDeadlineCalendar(view: View){
+    fun addNewDeadlineCalendar(view: View) {
         // Getting the necessary views
         val listView = findViewById<ListView>(R.id.calendar_deadline_listview)
         val editText = findViewById<TextInputEditText>(R.id.textInputEditCalendar)
@@ -39,7 +36,8 @@ class CalendarActivity : AppCompatActivity() {
 
         // Getting the entered text and the selected date
         val deadlineTitle = editText.text.toString()
-        val selectedDate = Instant.ofEpochMilli(calendar.date).atZone(ZoneId.systemDefault()).toLocalDate()
+        val selectedDate =
+            Instant.ofEpochMilli(calendar.date).atZone(ZoneId.systemDefault()).toLocalDate()
 
         // Add the new event to the deadline list
         val deadline = Deadline(deadlineTitle, DeadlineState.TODO, selectedDate)
