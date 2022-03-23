c4bf50e30da7025b2e7f29a9786e05eda4706e23
LennyDZ <lennydelzio@me.com>
2022-03-23 19:09:03 +0100

    move testing file to the right dir

---
+++ app/src/androidTest/java/com/github/multimatum_team/multimatum/DeadlineNotificationTest.kt
@@ -0,0 +1,21 @@
+package com.github.multimatum_team.multimatum
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.multimatum_team.multimatum.model.Deadline
+import com.github.multimatum_team.multimatum.model.DeadlineState
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.time.LocalDate
+
+class DeadlineNotificationTest {
+    @Test
+    fun `test`() {
+        Assert.assertEquals(1, 1)
+    }
+}
