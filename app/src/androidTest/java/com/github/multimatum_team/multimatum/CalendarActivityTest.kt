5bb3c26eeb9722ccf23adb5a203d6f61ed5585af
felko <baxbaxwalanuksiwe@gmail.com>
2022-03-24 22:22:23 +0100

    Inject mock deadline repository into CalendarActivityTest

--- app/src/androidTest/java/com/github/multimatum_team/multimatum/CalendarActivityTest.kt
+++ app/src/androidTest/java/com/github/multimatum_team/multimatum/CalendarActivityTest.kt
@@ -5,14 +5,32 @@ import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import org.hamcrest.Matchers.allOf
+import com.github.multimatum_team.multimatum.repository.DeadlineRepository
+import com.github.multimatum_team.multimatum.util.MockDeadlineRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import dagger.hilt.components.SingletonComponent
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
+@UninstallModules(RepositoryModule::class)
+@HiltAndroidTest
 class CalendarActivityTest {
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
     @get:Rule
     val activityRule = ActivityScenarioRule(CalendarActivity::class.java)
+
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+    }
 
     @Test
     fun textInputFieldCanBeClicked() {
@@ -28,4 +46,11 @@ class CalendarActivityTest {
             .perform(ViewActions.click())
     }
 
+    @Module
+    @InstallIn(SingletonComponent::class)
+    object TestDeadlineRepositoryModule {
+        @Provides
+        fun provideDeadlineRepository(): DeadlineRepository =
+            MockDeadlineRepository(listOf())
+    }
 }
