6d0db79958b9ff9735ec6ef8076cddca1c285f89
felko <baxbaxwalanuksiwe@gmail.com>
2022-03-29 12:31:52 +0200

    Update repository interface to use deadline IDs

--- app/src/main/java/com/github/multimatum_team/multimatum/repository/DeadlineRepository.kt
+++ app/src/main/java/com/github/multimatum_team/multimatum/repository/DeadlineRepository.kt
@@ -3,30 +3,37 @@ package com.github.multimatum_team.multimatum.repository
 import com.github.multimatum_team.multimatum.model.Deadline
 import java.time.LocalDate
 
+typealias DeadlineID = String
+
 /**
  * An interface for the deadline database.
- * A minimal implementation of this interface requires defining `fetchAll` and `put`.
+ * A minimal implementation of this interface requires defining `fetch`, `fetchAll` and `put`.
  * The other methods have a default implementation which may be overridden for performance purposes.
  */
 interface DeadlineRepository {
     /**
+     * Fetch a single deadline from its ID.
+     */
+    suspend fun fetch(id: String): Deadline?
+
+    /**
      * Fetch all user-defined deadlines from the repository.
      */
-    suspend fun fetchAll(): List<Deadline>
+    suspend fun fetchAll(): Map<DeadlineID, Deadline>
 
     /**
      * Fetch all deadlines occurring after a given date from the repository.
      */
-    suspend fun fetchAfter(dateLimit: LocalDate): List<Deadline> =
-        fetchAll().filter { it.date > dateLimit }
+    suspend fun fetchAfter(dateLimit: LocalDate): Map<DeadlineID, Deadline> =
+        fetchAll().filterValues { it.date > dateLimit }
 
     /**
-     * Add new deadline to the repository.
+     * Add a new deadline to the repository, returning the freshly generated ID of the new deadline.
      */
-    suspend fun put(deadline: Deadline)
+    suspend fun put(deadline: Deadline): DeadlineID
 
     /**
      * Add listener for database updates.
      */
-    fun onUpdate(callback: (List<Deadline>) -> Unit)
+    fun onUpdate(callback: (Map<DeadlineID, Deadline>) -> Unit)
 }
