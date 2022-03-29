14e9f323d0e3064d01448b009f40185a8bcbcc94
LennyDZ <lennydelzio@me.com>
2022-03-29 15:08:17 +0200

    add a test for broadcaster and move test file from AndroidTest->Test

--- app/src/test/java/com/github/multimatum_team/multimatum/ReminderBroadcastReceiverTest.kt
+++ app/src/test/java/com/github/multimatum_team/multimatum/ReminderBroadcastReceiverTest.kt
@@ -1,66 +1,41 @@
 package com.github.multimatum_team.multimatum
 
-import android.app.Notification
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import androidx.core.app.NotificationCompat
-import androidx.test.espresso.intent.Intents
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
-import org.junit.Assert
 import org.junit.Assert.assertEquals
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito
-import org.mockito.Mockito.mock
-import org.mockito.invocation.MockHandler
-import org.mockito.junit.MockitoJUnitRunner
-import org.mockito.kotlin.doNothing
-import org.mockito.kotlin.times
-import org.mockito.kotlin.verify
+import org.robolectric.Shadows.shadowOf
+import org.robolectric.shadows.ShadowNotificationManager
 
 @RunWith(AndroidJUnit4::class)
 class ReminderBroadcastReceiverTest {
 
-    private lateinit var mReminderBroadcastReceiver: ReminderBroadcastReceiver
-    private lateinit var mContext: Context
-    private lateinit var mockedNotificationService: NotificationManager
+    private lateinit var context: Context
 
     @Before
     @Throws(Exception::class)
     fun setUp() {
-        mReminderBroadcastReceiver = ReminderBroadcastReceiver()
-        mContext = mock(Context::class.java)
-        mockedNotificationService = mock(NotificationManager::class.java)
+        context = ApplicationProvider.getApplicationContext<Context>()
     }
 
-    @Test
-    fun emptyTest(){
-        assertEquals(1, 1)
-    }
-
-    /*
     /*
     * Test if the broadCast receiver launch a notification when "onReceive" is called
     * */
     @Test
     fun testNotificationLaunchOnReceive(){
-        Mockito.`when`(mContext.getSystemService(Context.NOTIFICATION_SERVICE)).thenReturn(mockedNotificationService)
+        val notificationManager =
+            ApplicationProvider.getApplicationContext<Context>().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val shadowNotificationManager: ShadowNotificationManager = shadowOf(notificationManager)
+        val reminderBroadcastReceiver: ReminderBroadcastReceiver = ReminderBroadcastReceiver()
+        reminderBroadcastReceiver.onReceive(context, Intent())
 
-        Mockito.`when`(PendingIntent.getActivity(Mockito.any(), Mockito.anyInt(), Mockito.any(), Mockito.anyInt())).thenReturn(Mockito.any())
-
-        doNothing().`when`(mockedNotificationService).notify(Mockito.anyInt(), Mockito.any())
-        mReminderBroadcastReceiver.onReceive(mContext, Intent())
-
-        verify(mockedNotificationService, times(1)).notify(Mockito.anyInt(), Mockito.any())
+        assertEquals(1, shadowNotificationManager.size())
     }
-     */
 
     /*
     @Test
