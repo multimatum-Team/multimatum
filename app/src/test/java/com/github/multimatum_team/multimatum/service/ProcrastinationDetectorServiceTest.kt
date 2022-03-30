e7c5dddcf2f6731e7c8226dcf24c2d2d1da65a0a
Valentin Aebi <valentin.aebi@epfl.ch>
2022-03-30 21:18:06 +0200

    Rename package procrastination_detector -> service to try to fix conflict

--- app/src/test/java/com/github/multimatum_team/multimatum/service/ProcrastinationDetectorServiceTest.kt
+++ app/src/test/java/com/github/multimatum_team/multimatum/service/ProcrastinationDetectorServiceTest.kt
@@ -1,4 +1,4 @@
-package com.github.multimatum_team.multimatum.procrastination_detector
+package com.github.multimatum_team.multimatum.service
 
 import android.content.Intent
 import android.content.SharedPreferences
