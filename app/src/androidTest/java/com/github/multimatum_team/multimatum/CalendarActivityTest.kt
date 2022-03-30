f2367fac4f43b0cba42d1dba5546416ea320a472
Léo Hauser <leo.hauser@epfl.ch>
2022-03-30 23:46:16 +0200

    Adding tests 1

--- app/src/androidTest/java/com/github/multimatum_team/multimatum/CalendarActivityTest.kt
+++ app/src/androidTest/java/com/github/multimatum_team/multimatum/CalendarActivityTest.kt
@@ -1,11 +1,14 @@
 package com.github.multimatum_team.multimatum
 
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.github.multimatum_team.multimatum.repository.DeadlineRepository
 import com.github.multimatum_team.multimatum.util.MockDeadlineRepository
+import com.github.multimatum_team.multimatum.viewmodel.DeadlineListViewModel
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,14 +16,18 @@ import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
 import dagger.hilt.components.SingletonComponent
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.kotlin.isNotNull
 import javax.inject.Singleton
 
 @UninstallModules(RepositoryModule::class)
 @HiltAndroidTest
 class CalendarActivityTest {
+    lateinit var deadlineList: DeadlineListViewModel
+
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
@@ -36,6 +43,16 @@ class CalendarActivityTest {
     fun textInputFieldCanBeClicked() {
         Espresso.onView(ViewMatchers.withId(R.id.textInputEditCalendar))
             .check(ViewAssertions.matches(ViewMatchers.isClickable()))
+    }
+
+    @Test
+    fun deadlineCanBeAddedUsingButton() {
+        Espresso.onView(ViewMatchers.withId(R.id.textInputEditCalendar))
+            .perform(click())
+            .perform(typeText("deadlineTestCase"))
+        Espresso.onView(ViewMatchers.withId(R.id.calendar_add_deadline_button))
+            .perform(click())
+        assertThat(deadlineList.getDeadlines().value, isNotNull())
     }
 
     @Module
