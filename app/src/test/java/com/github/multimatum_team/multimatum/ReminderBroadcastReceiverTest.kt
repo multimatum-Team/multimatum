76e5b9674d831f74677aee92da00ccaaf9edc1bb
LennyDZ <lennydelzio@me.com>
2022-03-30 13:21:54 +0200

    edit some comments

--- app/src/test/java/com/github/multimatum_team/multimatum/ReminderBroadcastReceiverTest.kt
+++ app/src/test/java/com/github/multimatum_team/multimatum/ReminderBroadcastReceiverTest.kt
@@ -37,30 +37,4 @@ class ReminderBroadcastReceiverTest {
         assertEquals(1, shadowNotificationManager.size())
     }
 
-    /*
-    @Test
-    fun testBroadcastReceiverSetTheNotification() {
-        val titleText = "foo"
-        val descrText = "foo is foo"
-        val id = 12
-        val intent = Intent(mContext, ReminderBroadcastReceiver::class.java) //this create an intent of broadcast receiver
-        //Adding extra parameter that will be used in the broadcast receiver to create the notification
-        intent.putExtra("title", titleText)
-        intent.putExtra("description", descrText)
-        intent.putExtra("id", id)
-        mReminderBroadcastReceiver!!.onReceive(mContext, intent)
-
-
-        //here we're looking for the notificationManager that have been
-        val notificationService: NotificationManager = mContext!!.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-
-        Assert.assertEquals(1, notificationService.activeNotifications.size)
-
-        val notification: Notification = notificationService.activeNotifications[0].notification
-
-        val contentIntent: PendingIntent = notification.contentIntent
-
-        Assert.assertEquals(contentIntent, MainActivity::class.java.name)
-    }*/
 }
