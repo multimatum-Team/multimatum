94a6142fcb9204dbb925d6467358f7afc8851de9
Voxanimus <louishenridanielpepito.dumas@epfl.ch>
2022-03-31 18:07:12 +0200

    test for account activity
    change test in account activity by considering some new branching

--- app/src/androidTest/java/com/github/multimatum_team/multimatum/AccountActivityTest.kt
+++ app/src/androidTest/java/com/github/multimatum_team/multimatum/AccountActivityTest.kt
@@ -1,19 +1,15 @@
 package com.github.multimatum_team.multimatum
 
-import android.content.Intent
-import androidx.test.core.app.ActivityScenario
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.matcher.IntentMatchers
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import org.hamcrest.Matchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -27,8 +23,21 @@ class AccountActivityTest {
     @Test
     fun launchSignInIntentWhenClickingButton(){
         Intents.init()
-        onView(withId(R.id.sign_in_button)).perform(ViewActions.click())
-        Intents.intended(IntentMatchers.toPackage("com.github.multimatum_team.multimatum"))
+        if (FirebaseAuth.getInstance().currentUser != null){
+            onView(withId(R.id.log_out_button)).perform(click())
+        }
+        onView(withId(R.id.sign_in_button)).perform(click())
+        Intents.intended(toPackage("com.github.multimatum_team.multimatum"))
         Intents.release()
     }
+
+    /*@Test
+    fun checkFragmentCreation(){
+        if (FirebaseAuth.getInstance().currentUser != null){
+            onView(withId(R.id.log_out_button)).perform(ViewActions.click())
+        }
+        onView(withId(R.id.sign_in_button)).perform(ViewActions.click())
+        Thread.sleep(2000)
+        assertNotNull(onView(withId(R.id.profile_frag)))
+    }*/
 }
