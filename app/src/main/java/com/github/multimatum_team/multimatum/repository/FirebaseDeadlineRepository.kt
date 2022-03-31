9718c79dbf6645e24b50783368f6156d40aee6c0
LennyDZ <lennydelzio@me.com>
2022-03-31 23:18:11 +0200

    fix firebaseRepo

--- app/src/main/java/com/github/multimatum_team/multimatum/repository/FirebaseDeadlineRepository.kt
+++ app/src/main/java/com/github/multimatum_team/multimatum/repository/FirebaseDeadlineRepository.kt
@@ -30,7 +30,11 @@ class FirebaseDeadlineRepository @Inject constructor() : DeadlineRepository {
         hashMapOf(
             "title" to deadline.title,
             "state" to deadline.state.ordinal,
-            "date" to deadline.dateTime
+            "date" to Timestamp(
+                deadline.dateTime
+                    .getLong(ChronoField.INSTANT_SECONDS),
+                0
+            )
         )
 
     /**
