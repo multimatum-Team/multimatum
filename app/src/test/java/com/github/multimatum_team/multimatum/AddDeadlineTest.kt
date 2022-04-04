2c8b5ce1bb64a511e1f6582f61cb0eef6c95646d
Florian Delavy <Florian.delavy@epfl.ch>
2022-04-04 15:40:51 +0200

    Modified AddDeadlineActivity and tests to select time

--- app/src/test/java/com/github/multimatum_team/multimatum/AddDeadlineTest.kt
+++ app/src/test/java/com/github/multimatum_team/multimatum/AddDeadlineTest.kt
@@ -1,10 +1,16 @@
 package com.github.multimatum_team.multimatum
 
-import android.content.Context
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.view.View
+import android.widget.TextView
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.contrib.PickerActions
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.multimatum_team.multimatum.repository.DeadlineRepository
@@ -17,12 +23,16 @@ import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
 import dagger.hilt.components.SingletonComponent
 import org.hamcrest.CoreMatchers
+import org.hamcrest.Matcher
 import org.hamcrest.MatcherAssert
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RuntimeEnvironment
+import org.robolectric.shadows.ShadowDatePickerDialog
+import org.robolectric.shadows.ShadowTimePickerDialog
 import org.robolectric.shadows.ShadowToast
 import javax.inject.Singleton
 
@@ -48,26 +58,69 @@ class AddDeadlineTest {
 
     @Test
     fun `The button should send a Toast if there is no title for the deadline`() {
-        Espresso.onView(ViewMatchers.withId(R.id.add_deadline_select_title))
+        onView(withId(R.id.add_deadline_select_title))
             .perform(ViewActions.replaceText(""))
         Espresso.closeSoftKeyboard()
-        Espresso.onView(ViewMatchers.withId(R.id.add_deadline_date_picker))
-            .perform(PickerActions.setDate(2022, 5, 8))
-        Espresso.onView(ViewMatchers.withId(R.id.add_deadline_button)).perform(ViewActions.click())
+        onView(withId(R.id.add_deadline_button)).perform(ViewActions.click())
         MatcherAssert.assertThat(ShadowToast.getTextOfLatestToast(),
             CoreMatchers.equalTo(RuntimeEnvironment.getApplication().applicationContext.getString(R.string.enter_a_title)))
     }
 
     @Test
-    fun `The button should add a deadline given it's title and its date`() {
-        Espresso.onView(ViewMatchers.withId(R.id.add_deadline_select_title))
+    fun `The button should add a deadline given a title, a date and a time`() {
+
+        // Select Title
+        onView(withId(R.id.add_deadline_select_title))
             .perform(ViewActions.replaceText("Test 1"))
         Espresso.closeSoftKeyboard()
-        Espresso.onView(ViewMatchers.withId(R.id.add_deadline_date_picker))
-            .perform(PickerActions.setDate(2022, 5, 8))
-        Espresso.onView(ViewMatchers.withId(R.id.add_deadline_button)).perform(ViewActions.click())
+
+        // Select Date
+        onView(withId(R.id.add_deadline_select_date))
+            .perform(ViewActions.click())
+        val dateDialog = ShadowDatePickerDialog.getLatestDialog() as DatePickerDialog
+        dateDialog.updateDate(2013, 10, 23)
+        dateDialog.getButton(DatePickerDialog.BUTTON_POSITIVE).performClick()
+
+        // Check if date is correctly selected
+        assertEquals("2013-11-23", getText(withId(R.id.add_deadline_text_date)))
+
+        // Select Time
+        onView(withId(R.id.add_deadline_select_time))
+            .perform(ViewActions.click())
+        val timeDialog = ShadowTimePickerDialog.getLatestDialog() as TimePickerDialog
+        timeDialog.updateTime(10,10)
+        timeDialog.getButton(TimePickerDialog.BUTTON_POSITIVE).performClick()
+
+        // Check if time is correctly selected
+        assertEquals("10:10", getText(withId(R.id.add_deadline_text_time)))
+
+        // Check if Toast correctly appear
+        onView(withId(R.id.add_deadline_button)).perform(ViewActions.click())
         MatcherAssert.assertThat(ShadowToast.getTextOfLatestToast(),
             CoreMatchers.equalTo(RuntimeEnvironment.getApplication().applicationContext.getString(R.string.deadline_created)))
+    }
+
+    /*
+    Matcher to recuperate text from TextView based on:
+    https://stackoverflow.com/questions/23381459/how-to-get-text-from-textview-using-espresso
+     */
+    private fun getText(matcher: Matcher<View?>?): String? {
+        val stringHolder = arrayOf<String?>(null)
+        onView(matcher).perform(object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isAssignableFrom(TextView::class.java)
+            }
+
+            override fun getDescription(): String {
+                return "getting text from a TextView"
+            }
+
+            override fun perform(uiController: UiController?, view: View) {
+                val tv = view as TextView //Save, because of check in getConstraints()
+                stringHolder[0] = tv.text.toString()
+            }
+        })
+        return stringHolder[0]
     }
 
     @Module
