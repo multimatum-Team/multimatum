96957533055efa68e8c968d5f9656d218ad6eee2
Florian Delavy <Florian.delavy@epfl.ch>
2022-04-05 17:15:47 +0200

    Added possibility of edition in AddDetailsActivity

--- app/src/main/java/com/github/multimatum_team/multimatum/DeadlineDetailsActivity.kt
+++ app/src/main/java/com/github/multimatum_team/multimatum/DeadlineDetailsActivity.kt
@@ -1,23 +1,21 @@
 package com.github.multimatum_team.multimatum
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import android.widget.TextView
+import android.text.SpannableStringBuilder
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import com.github.multimatum_team.multimatum.model.ClockServiceEntryPoint
 import com.github.multimatum_team.multimatum.model.Deadline
 import com.github.multimatum_team.multimatum.model.DeadlineState
 import com.github.multimatum_team.multimatum.repository.DeadlineID
 import com.github.multimatum_team.multimatum.service.ClockService
-import dagger.hilt.EntryPoint
-import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.EntryPointAccessors
-import dagger.hilt.components.SingletonComponent
-import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.Period
 import java.time.temporal.ChronoUnit
 import javax.inject.Inject
 
@@ -31,22 +29,108 @@ class DeadlineDetailsActivity : AppCompatActivity() {
     @Inject
     lateinit var clockService: ClockService
 
+    private var modifyMode: Boolean = true
+
+    private lateinit var titleView: EditText
+    private lateinit var dateView: TextView
+    private lateinit var detailView: TextView
+    private lateinit var doneButton: CheckBox
+
+    private lateinit var date: LocalDateTime
+    private lateinit var state: DeadlineState
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deadline_details)
 
         // Recuperate the Deadline from the intent
         val title = intent.getStringExtra(EXTRA_TITLE)
-        val date = intent.getSerializableExtra(EXTRA_DATE) as LocalDateTime
-        val state = intent.getSerializableExtra(EXTRA_STATE) as DeadlineState
+        date = intent.getSerializableExtra(EXTRA_DATE) as LocalDateTime
+        state = intent.getSerializableExtra(EXTRA_STATE) as DeadlineState
+
+        // Recuperate the necessary TextView
+        titleView = findViewById(R.id.deadline_details_activity_title)
+        dateView = findViewById(R.id.deadline_details_activity_date)
+        detailView = findViewById(R.id.deadline_details_activity_done_or_due)
+        doneButton = findViewById(R.id.deadline_details_activity_set_done)
 
         // Set the texts for the title and the date of the deadline
-        findViewById<TextView>(R.id.deadline_details_activity_title).text = title
-        findViewById<TextView>(R.id.deadline_details_activity_date).text =
-            getString(R.string.DueTheX, date)
+        titleView.text = SpannableStringBuilder(title)
+        dateView.text = getString(R.string.DueTheXatX, date.toLocalDate(), date.toLocalTime())
+
+        // Set the View to be unmodifiable at the start and remove displacement of the texts
+        fixArrangementView()
+
+        // Setup the CheckBox to set done
+        doneButton.isChecked = (state == DeadlineState.DONE)
+        doneButton.setOnCheckedChangeListener { _, isChecked ->
+            state = if (isChecked) DeadlineState.DONE else DeadlineState.TODO
+            updateDetail()
+        }
 
         // Set the detail text to inform the user if it is due, done or the remaining time
-        val detailView = findViewById<TextView>(R.id.deadline_details_activity_done_or_due)
+        updateDetail()
+
+    }
+
+
+    fun changeDateAndTime(view: View) {
+        selectDate()
+    }
+
+    fun goToModifyOrBack(view: View) {
+        if (modifyMode) {
+            editTitle(true)
+            findViewById<ImageButton>(R.id.deadline_details_activity_modify)
+                .setImageResource(android.R.drawable.checkbox_on_background)
+
+            dateView.setBackgroundResource(android.R.drawable.btn_default)
+            dateView.isClickable = true
+
+            doneButton.isClickable = true
+            doneButton.visibility = View.VISIBLE
+
+        } else {
+            editTitle(false)
+            findViewById<ImageButton>(R.id.deadline_details_activity_modify)
+                .setImageResource(android.R.drawable.ic_menu_manage)
+
+            dateView.isClickable = false
+            dateView.setBackgroundResource(android.R.color.transparent)
+
+            doneButton.isClickable = false
+            doneButton.visibility = View.GONE
+            //TODO: modify the deadline in the repository
+
+        }
+        modifyMode = modifyMode.not()
+
+    }
+
+    private fun editTitle(edit: Boolean) {
+        if (edit) {
+            titleView.isEnabled = true
+            titleView.setBackgroundResource(android.R.drawable.edit_text)
+            titleView.setTextColor(Color.BLACK)
+        } else {
+            titleView.isEnabled = false
+            titleView.setBackgroundResource(android.R.color.transparent)
+            titleView.setTextColor(Color.BLACK)
+        }
+
+    }
+
+    // Remove the displacement of texts that can happen
+    // and setup them for the creation of the Activity
+    private fun fixArrangementView() {
+        editTitle(true)
+        editTitle(false)
+        dateView.isClickable = false
+        dateView.setBackgroundResource(android.R.drawable.btn_default)
+        dateView.setBackgroundResource(android.R.color.transparent)
+    }
+
+    private fun updateDetail() {
         val actualDate = clockService.now()
         when {
             state == DeadlineState.DONE -> {
@@ -57,9 +141,61 @@ class DeadlineDetailsActivity : AppCompatActivity() {
             }
             else -> {
                 detailView.text =
-                    getString(R.string.DueInXDays, actualDate.until(date, ChronoUnit.DAYS).toString())
+                    getString(
+                        R.string.DueInXDays,
+                        actualDate.until(date, ChronoUnit.DAYS).toString()
+                    )
             }
         }
+    }
+
+    // Setup a DatePickerDialog that will select a date for the deadline and show it
+    private fun selectDate() {
+        // Set what will happen when a date is selected
+        val dateSetListener =
+            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                date = LocalDateTime.of(
+                    // monthOfYear is based on an enum that begin with 0
+                    // thus we need to increase it by one to have the true date
+                    year, monthOfYear + 1, dayOfMonth,
+                    date.hour, date.minute
+                )
+                selectTime()
+            }
+
+        // Prepare the datePickerDialog
+        val datePickerDialog = DatePickerDialog(
+            this, dateSetListener,
+            date.year, date.month.ordinal, date.dayOfMonth
+        )
+
+        // Show the Dialog on the screen
+        datePickerDialog.show()
+    }
+
+    //Setup a TimePickerDialog that will select a time for the deadline and show it
+    private fun selectTime() {
+        // Set what will happen when a time is selected
+        val timeSetListener =
+            TimePickerDialog.OnTimeSetListener { _, hoursOfDay, minutes ->
+                date = LocalDateTime.of(
+                    date.year, date.monthValue, date.dayOfMonth,
+                    hoursOfDay, minutes
+                )
+
+                dateView.text =
+                    getString(R.string.DueTheXatX, date.toLocalDate(), date.toLocalTime())
+                updateDetail()
+            }
+
+        // Prepare the datePickerDialog
+        val timePickerDialog = TimePickerDialog(
+            this, timeSetListener,
+            date.hour, date.minute, true
+        )
+
+        // Show the Dialog on the screen
+        timePickerDialog.show()
 
     }
 
