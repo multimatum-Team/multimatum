b943c76e873424c311e65ac3b64c5225aa5a77d3
felko <baxbaxwalanuksiwe@gmail.com>
2022-04-07 14:43:02 +0200

    Define User model

---
+++ app/src/main/java/com/github/multimatum_team/multimatum/model/User.kt
@@ -0,0 +1,12 @@
+package com.github.multimatum_team.multimatum.model
+
+typealias UserID = String
+
+abstract class User(open val id: UserID)
+
+data class AnonymousUser(override val id: UserID) : User(id)
+
+data class SignedInUser(
+    override val id: UserID,
+    val email: String
+) : User(id)
