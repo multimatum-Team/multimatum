de6ccdb82d55aaee9c1ad9f2eeee519afacec730
felko <baxbaxwalanuksiwe@gmail.com>
2022-04-07 19:54:52 +0200

    Add signIn before intent test

--- app/src/test/java/com/github/multimatum_team/multimatum/DeadlineRepositoryTest.kt
+++ app/src/test/java/com/github/multimatum_team/multimatum/DeadlineRepositoryTest.kt
@@ -4,13 +4,14 @@ import com.github.multimatum_team.multimatum.model.Deadline
 import com.github.multimatum_team.multimatum.model.DeadlineState
 import com.github.multimatum_team.multimatum.repository.DeadlineRepository
 import com.github.multimatum_team.multimatum.util.MockDeadlineRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import java.time.LocalDate
 import java.time.LocalDateTime
 
+@ExperimentalCoroutinesApi
 class DeadlineRepositoryTest {
     private val repository: DeadlineRepository = MockDeadlineRepository(
         listOf(
@@ -22,9 +23,18 @@ class DeadlineRepositoryTest {
 
     @Test
     fun `Default implementation of fetch returns the right deadline`() = runTest {
-        assertEquals(repository.fetch("0"), Deadline("Deadline 1", DeadlineState.DONE, LocalDateTime.of(2022, 3, 17, 0, 0)))
-        assertEquals(repository.fetch("1"), Deadline("Deadline 2", DeadlineState.DONE, LocalDateTime.of(2022, 3, 20, 0, 0)))
-        assertEquals(repository.fetch("2"), Deadline("Deadline 3", DeadlineState.TODO, LocalDateTime.of(2022, 4, 15, 0, 0)))
+        assertEquals(
+            repository.fetch("0"),
+            Deadline("Deadline 1", DeadlineState.DONE, LocalDateTime.of(2022, 3, 17, 0, 0))
+        )
+        assertEquals(
+            repository.fetch("1"),
+            Deadline("Deadline 2", DeadlineState.DONE, LocalDateTime.of(2022, 3, 20, 0, 0))
+        )
+        assertEquals(
+            repository.fetch("2"),
+            Deadline("Deadline 3", DeadlineState.TODO, LocalDateTime.of(2022, 4, 15, 0, 0))
+        )
     }
 
     @Test
@@ -33,8 +43,16 @@ class DeadlineRepositoryTest {
         val deadlines = runBlocking { repository.fetchAfter(date) }
         assertEquals(
             deadlines, mapOf(
-                "1" to Deadline("Deadline 2", DeadlineState.DONE, LocalDateTime.of(2022, 3, 20, 0, 0)),
-                "2" to Deadline("Deadline 3", DeadlineState.TODO, LocalDateTime.of(2022, 4, 15, 0 ,0)),
+                "1" to Deadline(
+                    "Deadline 2",
+                    DeadlineState.DONE,
+                    LocalDateTime.of(2022, 3, 20, 0, 0)
+                ),
+                "2" to Deadline(
+                    "Deadline 3",
+                    DeadlineState.TODO,
+                    LocalDateTime.of(2022, 4, 15, 0, 0)
+                ),
             )
         )
     }
