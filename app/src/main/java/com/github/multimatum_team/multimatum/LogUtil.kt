d13cb23b6a917953b00c039bf685204ca51a8c9b
Valentin Aebi <valentin.aebi@epfl.ch>
2022-04-12 21:35:27 +0200

    Use simple names as tags

--- app/src/main/java/com/github/multimatum_team/multimatum/LogUtil.kt
+++ app/src/main/java/com/github/multimatum_team/multimatum/LogUtil.kt
@@ -7,28 +7,32 @@ object LogUtil {
 
     fun debugLog(str: String) = safeExec {
         val currFunc = Thread.currentThread().stackTrace[STACK_IDX_FOR_ENV_FUNC]
-        Log.d(currFunc.className, str)
+        Log.d(createTag(currFunc), str)
     }
 
     fun logFunctionCall() = safeExec {
         val currFunc = Thread.currentThread().stackTrace[STACK_IDX_FOR_ENV_FUNC]
         val msg = "${currFunc.methodName} called"
-        Log.d("${Counter.next()} ${currFunc.className}", msg)
+        Log.d(createTag(currFunc), msg)
     }
 
     fun logFunctionCall(str: String) = safeExec {
         val currFunc = Thread.currentThread().stackTrace[STACK_IDX_FOR_ENV_FUNC]
         val msg = "${currFunc.methodName} called: $str"
-        Log.d("${Counter.next()} ${currFunc.className}", msg)
+        Log.d(createTag(currFunc), msg)
     }
 
-    private fun safeExec(task: () -> Unit){
+    private fun safeExec(task: () -> Unit) {
         try {
             task()
-        } catch (throwable: Throwable){
+        } catch (throwable: Throwable) {
             Log.d(TAG, "logging failed")
         }
     }
+
+    private fun createTag(currFunc: StackTraceElement): String =
+        "${Counter.next()} ${simpleNameOf(currFunc.className)}"
+    private fun simpleNameOf(name: String): String = name.takeLastWhile { it != '.' }
 
     private const val TAG = "DebugUtil"
     private const val STACK_IDX_FOR_ENV_FUNC = 6
