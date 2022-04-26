10ddc08ee6d165ea54cd53f74e143e21128944a5
felko <baxbaxwalanuksiwe@gmail.com>
2022-04-26 19:31:08 +0200

    Document User model

--- app/src/main/java/com/github/multimatum_team/multimatum/model/User.kt
+++ app/src/main/java/com/github/multimatum_team/multimatum/model/User.kt
@@ -2,10 +2,23 @@ package com.github.multimatum_team.multimatum.model
 
 typealias UserID = String
 
+/**
+ * Model for users.
+ * @param id an unique identifier provided by the user repository
+ */
 sealed class User(open val id: UserID)
 
+/**
+ * A model for users that have not yet signed-in using an e-mail.
+ * @param id an unique identifier provided by the user repository
+ */
 data class AnonymousUser(override val id: UserID) : User(id)
 
+/**
+ * A model for users that have signed-in with an e-mail.
+ * @param id an unique identifier provided by the user repository
+ * @param email the e-mail associated with the signed-in account
+ */
 data class SignedInUser(
     override val id: UserID,
     val email: String
