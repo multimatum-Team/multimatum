11099663f572290e2f2f5d558ef75fdf2e492cbf
Valentin Aebi <valentin.aebi@epfl.ch>
2022-04-26 21:49:58 +0200

    Implement Tokenizer for time info extraction

---
+++ app/src/main/java/com/github/multimatum_team/multimatum/model/datetime_parser/Tokens.kt
@@ -0,0 +1,78 @@
+package com.github.multimatum_team.multimatum.model.datetime_parser
+
+sealed interface Token {
+
+    val str: String
+
+    fun isEqualTo(text: String) = (str == text)
+
+    fun isHour(): Boolean = false
+    fun isMinute(): Boolean = false
+
+}
+
+data class AlphabeticToken(override val str: String) : Token {
+
+}
+
+data class NumericToken(override val str: String) : Token {
+    init {
+        require(str.all(Char::isDigit))
+    }
+    val numericValue: Int get() = str.toInt()
+    override fun isHour(): Boolean = (numericValue in 0..23)
+    override fun isMinute(): Boolean = (numericValue in 0..59)
+}
+
+data class SymbolToken(override val str: String) : Token {
+    init {
+        require(str.length == 1)
+    }
+    val charValue: Char get() = str[0]
+}
+
+object WhitespaceToken : Token {
+    override val str = " "
+    override fun toString(): String = "WhitespaceToken"
+}
+
+object Tokenizer {
+
+    private val TOKEN_CREATION_FUNCS = listOf(
+        Char::isLetter to ::AlphabeticToken,
+        Char::isDigit to ::NumericToken,
+        Char::isWhitespace to { _: String -> WhitespaceToken }
+    )
+
+    fun tokenize(str: String): List<Token> {
+
+        val tokens = mutableListOf<Token>()
+
+        fun consume(rem: String): Pair<Token?, String> =
+            if (rem.isEmpty()) {
+                Pair(null, "")
+            } else {
+                val headChar = rem[0]
+                val (matchChar, createToken) = TOKEN_CREATION_FUNCS.find { (matchChar, _) ->
+                    matchChar(headChar)
+                } ?: Pair({ _: Char -> false }, ::SymbolToken)
+                val tailChars = rem.substring(1)
+                val tokenTextTail = tailChars.takeWhile(matchChar)
+                val newRem = tailChars.dropWhile(matchChar)
+                val s = headChar + tokenTextTail
+                Pair(createToken(s), newRem)
+            }
+
+        tailrec fun tokenizeRemaining(rem: String) {
+            if (rem.isNotEmpty()) {
+                val (token, newRem) = consume(rem)
+                tokens.add(token!!)
+                tokenizeRemaining(newRem)
+            }
+        }
+
+        tokenizeRemaining(str)
+        return tokens
+    }
+
+}
