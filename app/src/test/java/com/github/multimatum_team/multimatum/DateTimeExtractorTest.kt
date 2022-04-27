fa0e606c5a3e883e4c7e4d75d210ecd5a7bf1092
Valentin Aebi <valentin.aebi@epfl.ch>
2022-04-27 17:05:51 +0200

    Tests for am-pm notation and titles with 2 times

--- app/src/test/java/com/github/multimatum_team/multimatum/DateTimeExtractorTest.kt
+++ app/src/test/java/com/github/multimatum_team/multimatum/DateTimeExtractorTest.kt
@@ -8,13 +8,46 @@ import java.time.LocalTime
 class DateTimeExtractorTest {
 
     @Test
-    fun date_is_extracted_correctly(){
+    fun time_is_extracted_correctly(){
         val str = "Aqua-pony at 10:00"
-        val expText = "Aqua-pony at"
+        val expText = "Aqua-pony"
         val actualRes = DateTimeExtractor.parse(str)
         assertEquals(expText, actualRes.text)
         assertTrue(actualRes.timeFound)
         assertEquals(LocalTime.of(10, 0), actualRes.time)
+        assertFalse(actualRes.dateFound)
+    }
+
+    @Test
+    fun am_time_is_parsed_correctly(){
+        val str = "Chemistry 2am (report)"
+        val expText = "Chemistry (report)"
+        val actualRes = DateTimeExtractor.parse(str)
+        assertEquals(expText, actualRes.text)
+        assertTrue(actualRes.timeFound)
+        assertEquals(LocalTime.of(2, 0), actualRes.time)
+        assertFalse(actualRes.dateFound)
+    }
+
+    @Test
+    fun pm_time_is_parsed_correctly(){
+        val str = "History dissertation 4pm"
+        val expText = "History dissertation"
+        val actualRes = DateTimeExtractor.parse(str)
+        assertEquals(expText, actualRes.text)
+        assertTrue(actualRes.timeFound)
+        assertEquals(LocalTime.of(16, 0), actualRes.time)
+        assertFalse(actualRes.dateFound)
+    }
+
+    @Test
+    fun second_time_is_ignored(){
+        val str = "History dissertation 4pm 5pm"
+        val expText = "History dissertation 5pm"
+        val actualRes = DateTimeExtractor.parse(str)
+        assertEquals(expText, actualRes.text)
+        assertTrue(actualRes.timeFound)
+        assertEquals(LocalTime.of(16, 0), actualRes.time)
         assertFalse(actualRes.dateFound)
     }
 
