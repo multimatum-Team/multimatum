23a0b6bdeca4567885e3e05def1307e775549a41
Valentin Aebi <valentin.aebi@epfl.ch>
2022-05-01 11:44:20 +0200

    Attempt to fix cognitive complexity warning

--- app/src/main/java/com/github/multimatum_team/multimatum/model/datetime_parser/DateTimeExtractor.kt
+++ app/src/main/java/com/github/multimatum_team/multimatum/model/datetime_parser/DateTimeExtractor.kt
@@ -82,9 +82,9 @@ class DateTimeExtractor(private val dateTimePatterns: DateTimePatterns) {
         date: LocalDate?,
         time: LocalTime?,
         alreadyProcessedTokensList: MutableList<Token>
-    ): Pair<LocalDate?, LocalTime?> =
+    ): Pair<LocalDate?, LocalTime?> {
         if (remTokens.isEmpty()) {
-            Pair(date, time)  // end of list, return found info
+            return Pair(date, time)  // end of list, return found info
         } else {
             val (extractedInfo, newRemTokens, consumed) = extractDateTimeInfo(remTokens)
             when (extractedInfo) {
@@ -92,20 +92,21 @@ class DateTimeExtractor(private val dateTimePatterns: DateTimePatterns) {
                     // if a date has already been found, ignore the newly found one (treat it as normal text)
                     alreadyProcessedTokensList.addAll(if (date == null) listOf(RemovedToken) else consumed)
                     val newDate = date ?: extractedInfo.date
-                    recursivelyParse(newRemTokens, newDate, time, alreadyProcessedTokensList)
+                    return recursivelyParse(newRemTokens, newDate, time, alreadyProcessedTokensList)
                 }
                 is ExtractedTime -> {
                     // if a time has already been found, ignore the newly found one (treat it as normal text)
                     alreadyProcessedTokensList.addAll(if (time == null) listOf(RemovedToken) else consumed)
                     val newTime = time ?: extractedInfo.time
-                    recursivelyParse(newRemTokens, date, newTime, alreadyProcessedTokensList)
+                    return recursivelyParse(newRemTokens, date, newTime, alreadyProcessedTokensList)
                 }
                 is NoInfo -> {
                     alreadyProcessedTokensList.addAll(consumed)
-                    recursivelyParse(newRemTokens, date, time, alreadyProcessedTokensList)
+                    return recursivelyParse(newRemTokens, date, time, alreadyProcessedTokensList)
                 }
             }
         }
+    }
 
     /**
      * Set followedByWhitespace to true for each token that is followed by a WhitespaceToken
