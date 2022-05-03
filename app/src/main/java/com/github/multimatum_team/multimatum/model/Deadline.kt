e7ed961e0e76e21da78b5dab39f8aa91a92e529b
felko <baxbaxwalanuksiwe@gmail.com>
2022-05-03 19:43:47 +0200

    Add owner attribute to the deadline model

--- app/src/main/java/com/github/multimatum_team/multimatum/model/Deadline.kt
+++ app/src/main/java/com/github/multimatum_team/multimatum/model/Deadline.kt
@@ -19,6 +19,21 @@ enum class DeadlineState {
 }
 
 /**
+ * Defines which entity can own a deadline.
+ */
+sealed interface DeadlineOwner
+
+/**
+ * A value that represents the fact that the deadline is owned by the user.
+ */
+object UserOwned : DeadlineOwner
+
+/**
+ * Represents a deadline that belongs to a group.
+ */
+data class GroupOwned(val groupID: GroupID) : DeadlineOwner
+
+/**
  * A deadline.
  *
  * The Deadline class represents a deadline at a given date.
@@ -30,6 +45,7 @@ enum class DeadlineState {
  * @property dateTime the time at which the work is due
  * @property description some description of the deadline, by default empty
  * @property notificationsTimes ArrayList of the notification's trigger time before deadline
+ * @property owner the owner of the deadline
  * @constructor Creates a deadline from specified parameters
  * @throws IllegalArgumentException when title is empty or startDate > end
  */
@@ -39,6 +55,7 @@ data class Deadline(
     val dateTime: LocalDateTime,
     val description: String = "",
     val notificationsTimes: List<Long> = ArrayList()
+    val owner: DeadlineOwner = UserOwned
 ) {
     init {
         if (title.isEmpty()) {
