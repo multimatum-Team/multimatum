a6b82139723af1a33158d3987ec1ad2283de0a4a
Voxanimus <louishenridanielpepito.dumas@epfl.ch>
2022-05-17 13:09:47 +0200

    Add group creation feature

--- app/src/main/java/com/github/multimatum_team/multimatum/activity/GroupsActivity.kt
+++ app/src/main/java/com/github/multimatum_team/multimatum/activity/GroupsActivity.kt
@@ -1,14 +1,20 @@
 package com.github.multimatum_team.multimatum.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.ListView
 import androidx.activity.viewModels
-import com.github.multimatum_team.multimatum.LogUtil
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.github.multimatum_team.multimatum.R
 import com.github.multimatum_team.multimatum.adaptater.UserGroupAdapter
 import com.github.multimatum_team.multimatum.repository.UserRepository
 import com.github.multimatum_team.multimatum.viewmodel.GroupViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -27,6 +33,11 @@ class GroupsActivity : AppCompatActivity() {
 
         val adapter = UserGroupAdapter(this, groupViewModel, userRepository)
 
+        val button = findViewById<FloatingActionButton>(R.id.addGroupButton)
+        button.setOnClickListener{ view ->
+            addGroup(button)
+        }
+
         // Set when you maintain your finger on an item of the list, launch the detail activity
         listView.setOnItemClickListener { _, _, position, _ ->
             val group = adapter.getItem(position)
@@ -39,6 +50,40 @@ class GroupsActivity : AppCompatActivity() {
         listView.adapter = adapter
         groupViewModel.getGroups().observe(this){groups ->
             adapter.setGroups(groups)
+        }
+    }
+
+    private fun addGroup(view: View){
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("Create group")
+        alertDialogBuilder.setMessage("Group name :")
+
+        //add editable text box for group name
+        val input = EditText(this@GroupsActivity)
+        val lp = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+        input.layoutParams = lp
+        alertDialogBuilder.setView(input)
+
+        //add cancel and create button
+        alertDialogBuilder.setCancelable(false)
+        alertDialogBuilder.setPositiveButton("Create", DialogInterface.OnClickListener { dialog, _ ->
+                createGroup(dialog, input)
+            })
+        alertDialogBuilder.setNegativeButton("Cancel", DialogInterface.OnClickListener{
+            dialog, _ -> dialog.cancel()
+        })
+
+        alertDialogBuilder.create()
+        alertDialogBuilder.show()
+    }
+
+    private fun createGroup(dialog: DialogInterface, editText: EditText) {
+        groupViewModel.createGroup(editText.text.toString()) { groupID ->
+            GroupDetailsActivity.newIntent(this, groupID)
+            startActivity(intent)
         }
     }
 }
