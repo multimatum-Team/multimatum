bb806a6444246d4a8d73d0688b38140dbfdd05e2
LennyDZ <lennydelzio@me.com>
2022-05-19 08:59:59 +0200

    fix mock deadlineRepo

--- app/src/test/java/com/github/multimatum_team/multimatum/util/MockPdfRepository.kt
+++ app/src/test/java/com/github/multimatum_team/multimatum/util/MockPdfRepository.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.net.Uri
 import com.github.multimatum_team.multimatum.repository.PdfRepository
 import org.mockito.kotlin.doNothing
+import java.io.File
 
 class MockPdfRepository: PdfRepository() {
     override fun uploadPdf(data: Uri, context: Context, callback: (String) -> Unit) {
@@ -11,6 +12,10 @@ class MockPdfRepository: PdfRepository() {
     }
 
     override fun delete(path: String) {
+        doNothing()
+    }
+
+    override fun downloadPdf(path: String, title: String, callback: (File) -> Unit) {
         doNothing()
     }
 }
