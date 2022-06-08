d54d713a1ca96769cf4e5aade4e3f3218aaedc6b
LennyDZ <lennydelzio@me.com>
2022-06-08 21:44:06 +0200

    rm log import

--- app/src/main/java/com/github/multimatum_team/multimatum/activity/AddDeadlineActivity.kt
+++ app/src/main/java/com/github/multimatum_team/multimatum/activity/AddDeadlineActivity.kt
@@ -8,7 +8,6 @@ import android.app.TimePickerDialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 import android.widget.ProgressBar
